0ba2b87b2dae53f8d7e941d252c3813cfd08c6fc
m0z4rt <PiterParker1988@gmail.com>
2014-11-28 21:23:22 +0300

    Minor fixes

--- app/controllers/expense_categories_controller.rb
+++ app/controllers/expense_categories_controller.rb
@@ -4,7 +4,7 @@ class ExpenseCategoriesController < ApplicationController
   respond_to :html, :js
 
   def index
-    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 20)
+    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 15)
   end
 
   def show
@@ -17,7 +17,7 @@ class ExpenseCategoriesController < ApplicationController
   end
 
   def create
-    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 20)
+    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 15)
     @expense_category = ExpenseCategory.create(expense_category_params)
   end
 
@@ -26,7 +26,7 @@ class ExpenseCategoriesController < ApplicationController
   end
 
   def update
-    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 20)
+    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 15)
     @expense_category = ExpenseCategory.find(params[:id])
     @expense_category.update_attributes(expense_category_params)
   end
@@ -36,7 +36,7 @@ class ExpenseCategoriesController < ApplicationController
   end
 
   def destroy
-    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 20)
+    @expense_categories = ExpenseCategory.where("account_id = #{current_account.id}").paginate(page: params[:page], per_page: 15)
     @expense_category = ExpenseCategory.find(params[:id])
     @expense_category.destroy
   end
