e8d7391e4dbd5cd4e1e89b50d976e516dfaf87c8
m0z4rt <PiterParker1988@gmail.com>
2014-11-28 22:57:27 +0300

    Add pagination

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -4,7 +4,7 @@ class AccountingController < ApplicationController
   respond_to :html, :js
 
   def index
-    @records = Account.find_by_sql("
+    sql_record = "
         SELECT expenses.name, expenses.expense AS record, expenses.created_at, expenses.updated_at, expenses.expense_category_id AS category_id
         FROM expenses
         WHERE expenses.account_id = #{current_account.id}
@@ -12,7 +12,8 @@ class AccountingController < ApplicationController
         SELECT incomes.name, incomes.income, incomes.created_at, incomes.updated_at, incomes.income_category_id
         FROM incomes
         WHERE incomes.account_id = #{current_account.id}
-        ORDER BY updated_at DESC")
+        ORDER BY updated_at DESC"
+    @records = Account.paginate_by_sql(sql_record, page: params[:page], per_page: 15)
     @total_incomes = Income.where("account_id = #{current_account.id}").sum(:income)
     @total_expenses = Expense.where("account_id = #{current_account.id}").sum(:expense)
   end
