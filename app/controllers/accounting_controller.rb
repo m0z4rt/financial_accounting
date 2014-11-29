e0d48622f37bd7c4dd969cdde0dd99e1ece75d5c
m0z4rt <PiterParker1988@gmail.com>
2014-11-29 15:41:42 +0300

    Improve sql_records for show Categories in Accounting

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -5,15 +5,21 @@ class AccountingController < ApplicationController
 
   def index
     sql_record = "
-        SELECT expenses.name, expenses.expense AS record, expenses.created_at, expenses.updated_at,
-        expenses.record_type, expenses.expense_category_id AS category_id
-        FROM expenses
-        WHERE expenses.account_id = #{current_account.id}
-        UNION
-        SELECT incomes.name, incomes.income, incomes.created_at, incomes.updated_at, incomes.record_type,
-        incomes.income_category_id
+        SELECT incomes.name, incomes.income AS record, incomes.created_at, incomes.updated_at,
+        incomes.record_type, income_categories.name AS Category
         FROM incomes
+        INNER JOIN
+        income_categories
+        ON incomes.income_category_id = income_categories.id
         WHERE incomes.account_id = #{current_account.id}
+        UNION
+        SELECT expenses.name, expenses.expense AS record, expenses.created_at, expenses.updated_at,
+        expenses.record_type, expense_categories.name AS Category
+        FROM expenses
+        INNER JOIN
+        expense_categories
+        ON expenses.expense_category_id = expense_categories.id
+        WHERE expenses.account_id = #{current_account.id}
         ORDER BY updated_at DESC"
     @records = Account.paginate_by_sql(sql_record, page: params[:page], per_page: 15)
     @total_incomes = Income.where("account_id = #{current_account.id}").sum(:income)
