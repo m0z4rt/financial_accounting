a7dcffb536dcf3e9c2b17c3a7e9c90a66d995ff5
m0z4rt <PiterParker1988@gmail.com>
2014-12-06 22:36:51 +0300

    Improve deleting categories

--- app/models/expense_category.rb
+++ app/models/expense_category.rb
@@ -1,4 +1,5 @@
 class ExpenseCategory < ActiveRecord::Base
   belongs_to :account
+  has_many :expenses, :dependent => :destroy
   validates :name, presence: true
 end
--- app/models/income_category.rb
+++ app/models/income_category.rb
@@ -1,4 +1,5 @@
 class IncomeCategory < ActiveRecord::Base
   belongs_to :account
+  has_many :incomes, :dependent => :destroy
   validates :name, presence: true
 end
