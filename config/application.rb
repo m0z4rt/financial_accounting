e02e5a8ba93e1a8f197de3aa05c210e3c1a7536a
m0z4rt <PiterParker1988@gmail.com>
2014-12-07 20:50:28 +0300

    Fix migrate create records view

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module FinancialAccounting
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.active_record.schema_format = :sql
   end
 end
--- db/migrate/20141207130456_create_records_view.rb
+++ db/migrate/20141207130456_create_records_view.rb
@@ -1,15 +1,15 @@
 class CreateRecordsView < ActiveRecord::Migration
   def change
     execute <<-SQL
-    CREATE OR REPLACE VIEW records AS
-    SELECT incomes.id as record_id, incomes.name, incomes.income AS record, incomes.account_id,
+    CREATE OR REPLACE VIEW incomes_expenses_view AS
+    SELECT incomes.id as record_id, incomes.name, incomes.income AS record_value, incomes.account_id,
     incomes.created_at, incomes.updated_at, incomes.record_type, income_categories.name AS Category
     FROM incomes
     LEFT OUTER JOIN
     income_categories
     ON incomes.income_category_id = income_categories.id
     UNION
-    SELECT expenses.id  as record_id, expenses.name, expenses.expense AS record, expenses.account_id,
+    SELECT expenses.id  as record_id, expenses.name, expenses.expense AS record_value, expenses.account_id,
     expenses.created_at, expenses.updated_at, expenses.record_type, expense_categories.name AS Category
     FROM expenses
     LEFT OUTER JOIN
