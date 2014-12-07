827ef035340b000fb58b7076dd030103f59c0f6a
m0z4rt <PiterParker1988@gmail.com>
2014-12-07 20:28:47 +0300

    Add view for Heroku

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,10 +2,4 @@ class Record < ActiveRecord::Base
   self.table_name = 'incomes_expenses_view'
 
   belongs_to :account
-
-  def self.search(search)
-    if search
-      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-    end
-  end
 end
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module FinancialAccounting
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.active_record.schema_format = :sql
   end
 end
